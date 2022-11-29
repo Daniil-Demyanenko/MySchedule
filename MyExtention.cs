c4e2375686373d2bc1aea141a1af330e569c4688
Daniil Demyanenko <daniil-demyanenko1@ya.ru>
2022-11-30 00:56:51 +0300

    Добавлен класс расширений

---
+++ MyExtention.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace job_checker
+{
+    public static class MyExtentions
+    {
+        private static bool ContainsDay(this string str)
+        {
+            var days = new string[] { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота" };
+            foreach (var day in days)
+                if (str.Contains(day, StringComparison.InvariantCultureIgnoreCase)) return true;
+
+            return false;
+        }
+    }
+}
