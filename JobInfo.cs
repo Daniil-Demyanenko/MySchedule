4cdf67537ed50b3eeb6934fd0ec2915d4340c5e2
Daniil Demyanenko <daniil-demyanenko1@ya.ru>
2023-01-24 10:53:35 +0300

    JobParser переименован в CoupleSchedule,  пересмотрены методы

--- JobInfo.cs
+++ JobInfo.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace job_checker;
+
+/// <summary>
+/// Описание пары
+/// </summary>
+/// <value></value>
 public record JobInfo
 {
     /// <summary>
