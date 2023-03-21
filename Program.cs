472d07b7682aefc43fbd55aa7c5effac0406670e
4firerain4 <dr34m.runn3r@gmail.com>
2023-03-22 00:21:56 +0300

    Реализована проверка релевантности кэша

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ class Program
     static void Main(string[] args)
     {
         CoupleSchedule.Update();
+        // scheduleDownloader.CheckUpdate();
         Console.ReadKey();
     }
 }
---
+++ scheduleDownloader.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+
+namespace job_checker
+{
+    public static class scheduleDownloader
+    {
+        private static string dir = AppDomain.CurrentDomain.BaseDirectory + "/Cache"; //Путь к папке Cache в директории программы
+        public static void CheckUpdate()
+        {
+            if (!CacheIsRelevant()) Download();
+        }
+
+        private static bool CacheIsRelevant()
+        {
+
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+                return false;
+            }
+
+            if (!File.Exists(dir + "/ras.xls"))
+            {
+                return false;
+            }
+
+            var fileWriteDate = File.GetLastWriteTime(dir + "/ras.xls");
+            if ((DateTime.Now - fileWriteDate).TotalHours > 6) return false;
+
+            return true;
+
+        }
+
+        private static void Download()
+        {
+
+        }
+    }
+}
