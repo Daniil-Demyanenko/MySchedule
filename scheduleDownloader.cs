f27b698d0b6224428f9013052b9e09f5226e84f5
4firerain4 <dr34m.runn3r@gmail.com>
2023-04-01 19:08:23 +0300

    Реализована заргузка расписаний ИФМОИОТ ОФО БАК

--- scheduleDownloader.cs
+++ scheduleDownloader.cs
@@ -1,11 +1,19 @@
 using System;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace job_checker
 {
     public static class ScheduleDownloader
     {
-        private static string dir = AppDomain.CurrentDomain.BaseDirectory + "/Cache"; //Путь к папке Cache в директории программы
+        private static readonly HttpClient _client = new HttpClient();
+        private static string _dir = AppDomain.CurrentDomain.BaseDirectory; //Путь к директории программы
+        private static string _cdir = AppDomain.CurrentDomain.BaseDirectory + "/Cache"; //Путь к папке Cache в директории программы
         public static void CheckUpdate()
         {
             if (!CacheIsRelevant()) Download();
@@ -14,27 +22,84 @@ namespace job_checker
         private static bool CacheIsRelevant()
         {
 
-            if (!Directory.Exists(dir))
+            if (!Directory.Exists(_cdir))
             {
-                Directory.CreateDirectory(dir);
+                Directory.CreateDirectory(_cdir);
                 return false;
             }
 
-            if (!File.Exists(dir + "/ras.xls"))
+            if (!File.Exists(_cdir + "/ras.xls"))
             {
                 return false;
             }
 
-            var fileWriteDate = File.GetLastWriteTime(dir + "/ras.xls");
+            var fileWriteDate = File.GetLastWriteTime(_cdir + "/ras.xls");
             if ((DateTime.Now - fileWriteDate).TotalHours > 6) return false;
 
             return true;
 
         }
-
         private static void Download()
         {
+            (string name, string link) = GetDirectLinkAsync(ParseLinkFromPage()).Result;
+            Console.WriteLine($"{name}\n{link}");
+            string extension = name.Split('.')[^1]; // получаем расширение файла (xls или xlsx)
+            string filePath = _cdir + "/ИФМОИОТ_ОФО." + extension; // генерируем имя скачанного файла
+            DownloadFromDirectLinkAsync(link, filePath).Wait(); // ждём окончания загрузки
+            Console.WriteLine($"Файл успешно скачан по пути {filePath}");
 
+            Console.ReadKey();
+        }
+        private static string ParseLinkFromPage()
+        {
+            var link = LoadPage(@"https://lgpu.org/elektronnyy-resurs-distancionnogo-obucheniya-ifmit.html");
+            var document = new HtmlDocument();
+            document.LoadHtml(link);
+
+            HtmlNodeCollection xpathLink = document.DocumentNode.SelectNodes("//tr[3]/td[2]/p/a");
+            var diskLink = "";
+            diskLink = xpathLink[0].GetAttributeValue("href", "");
+
+            return diskLink;
+        }
+
+        private static string LoadPage(string url) //Тут происходит какая то чёрная магия, в которой я не разобрался. Оно работает - и хуй с ним
+        {
+            var result = "";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var receiveStream = response.GetResponseStream();
+                if (receiveStream != null)
+                {
+                    StreamReader readStream;
+                    if (response.CharacterSet == null)
+                    {
+                        readStream = new StreamReader(receiveStream);
+                    }
+                    else readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    result = readStream.ReadToEnd();
+                    readStream.Close();
+                }
+                response.Close();
+            }
+            return result;
+        }
+
+        static async Task<(string, string)> GetDirectLinkAsync(string url)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, $"https://cloud-api.yandex.net/v1/disk/public/resources?public_key={url}");
+            using var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            dynamic result = JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
+            return (result.name, result.file);
+        }
+        private static async Task DownloadFromDirectLinkAsync(string url, string path)
+        {
+            using WebClient webClient = new WebClient();
+            await webClient.DownloadFileTaskAsync(new Uri(url), path); // если директория не существует то ее надо создать заранее
         }
     }
 }
