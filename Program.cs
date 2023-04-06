345dc0610e8e3e4723a94549b81b61348dec9006
Daniil Demyanenko <daniil-demyanenko1@ya.ru>
2023-04-06 22:58:51 +0300

    Проверка расписаний по таймеру

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-
+using System.Timers;
 
 namespace job_checker;
 class Program
@@ -9,7 +9,7 @@ class Program
     {
         TelegramBot bot = new TelegramBot(args[0]);
         CoupleSchedule.Update();
-        // scheduleDownloader.CheckUpdate();
+        // SetUpdateScheduleTimer(); //Запускаем таймер, проверяющий обновление расписаний каждые 4 часа
         bot.WaitMessages();
 
 
@@ -24,6 +24,16 @@ class Program
         }
         Console.WriteLine($"кол-во пар {CoupleSchedule.Couples.Count()}");
         Console.ReadKey();
+    }
+
+
+    static void SetUpdateScheduleTimer()
+    {
+        var UpdateInterval = new TimeSpan(hours: 4, minutes: 0, seconds: 0);
+        var UpdateTimer = new Timer(UpdateInterval);
+        UpdateTimer.Elapsed += (s, e) => ScheduleDownloader.CheckUpdate();
+        UpdateTimer.AutoReset = true;
+        UpdateTimer.Enabled = true;
     }
 }
 
