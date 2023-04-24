9af4f97c787b81fe2b3ce564bc82dab214bb5b6e
Daniil Demyanenko <daniil-demyanenko1@ya.ru>
2023-04-25 00:14:33 +0300

    Пути расписаний для тестов вынесены в отдельную структуру

---
+++ UnitTests/ParserCouplesUnitTests.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using job_checker.InstituteParsers;
+using Xunit;
+
+namespace job_checker.UnitTests;
+
+public class ParserCouplesUnitTests
+{
+    // static string _OFOBAK1 = "UnitTests/TESTSDATA/OFOBAK1.xlsx";
+    // static string _OFOBAK2 = "UnitTests/TESTSDATA/OFOBAK2.xlsx";
+    // static string _OFOBAK3 = "UnitTests/TESTSDATA/OFOBAK3.xlsx";
+    // static string _OFOMAG = "UnitTests/TESTSDATA/OFOMAG.xlsx";
+    // static string _ZFOBAK = "UnitTests/TESTSDATA/ZFOBAK.xls";
+    // static string _ZFOMAG = "UnitTests/TESTSDATA/ZFOMAG.xls";
+
+    [Fact]
+    public void ParsingTestOFOBAK1_GroupCount()
+    {
+        var couples = GetCouples(Paths.OFOBAK1);
+        var groups = couples.Select(x => x.Course + " " + x.Group).Distinct();
+
+        Assert.Equal(40, groups.Count());
+    }
+
+    [Fact]
+    public void ParsingTestOFOBAK1_CoupleCount()
+    {
+        var couples = GetCouples(Paths.OFOBAK1);
+        var POMII1 = couples.Where(x => x.Course == 1 && x.Group == "ПО (МиИ)");
+
+        Assert.Equal(18, POMII1.Count());
+    }
+
+    [Fact]
+    public void ParsingTestOFOBAK2_GroupCount()
+    {
+        var couples = GetCouples(Paths.OFOBAK2);
+        var groups = couples.Select(x => x.Course + " " + x.Group).Distinct();
+
+        Assert.Equal(43, groups.Count());
+    }
+
+    [Fact]
+    public void ParsingTestOFOBAK2_CoupleCount()
+    {
+        var couples = GetCouples(Paths.OFOBAK2);
+        var POMII1 = couples.Where(x => x.Course == 1 && x.Group == "ПО (МиИ)");
+
+        Assert.Equal(19, POMII1.Count());
+    }
+
+    [Fact]
+    public void ParsingTestOFOBAK3_GroupCount()
+    {
+        var couples = GetCouples(Paths.OFOBAK3);
+        var groups = couples.Select(x => x.Course + " " + x.Group).Distinct();
+
+        Assert.Equal(43, groups.Count());
+    }
+
+    [Fact]
+    public void ParsingTestOFOBAK3_CoupleCount()
+    {
+        var couples = GetCouples(Paths.OFOBAK3);
+        var POMII1 = couples.Where(x => x.Course == 1 && x.Group == "ПО (МиИ)");
+
+        Assert.Equal(21, POMII1.Count());
+    }
+
+    [Fact]
+    public void ParsingTestOFOMAG_GroupCount()
+    {
+        var couples = GetCouples(Paths.OFOMAG);
+        var groups = couples.Select(x => x.Course + " " + x.Group).Distinct();
+
+        int count = groups.Count();
+        Assert.Equal(17, count);
+    }
+
+    [Fact]
+    public void ParsingTestOFOMAG_CoupleCount()
+    {
+        var couples = GetCouples(Paths.OFOMAG);
+        var POMII1 = couples.Where(x => x.Course == 1 && x.Group == "маг ПО (ТО) []");
+
+        int count = POMII1.Count();
+        Assert.Equal(18, count);
+    }
+
+    [Fact]
+    public void ParsingTestZFOMAG_GroupCount()
+    {
+        var couples = GetCouples(Paths.ZFOMAG);
+        var groups = couples.Select(x => x.Course + " " + x.Group).Distinct();
+
+        int count = groups.Count();
+        Assert.Equal(4, count);
+    }
+
+    [Fact]
+    public void ParsingTestZFOMAG_CoupleCount()
+    {
+        var couples = GetCouples(Paths.ZFOMAG);
+        var POPT1 = couples.Where(x => x.Course == 1 && x.Group == "маг ПО (ПТ)");
+
+        Assert.Equal(18, POPT1.Count());
+    }
+
+    // [Fact]
+    // public void ParsingTestZFOBAK_GroupCount()
+    // {
+    //     var couples = GetZFOCouples(_ZFOBAK);
+    //     var groups = couples.Select(x => x.Course + " " + x.Group).Distinct();
+
+    //     Assert.Equal(11, groups.Count());
+    // }
+
+    // [Fact]
+    // public void ParsingTestZFOBAK_CoupleCount()
+    // {
+    //     var couples = GetZFOCouples("UnitTests/TESTSDATA/ZFOBAK.xls");
+    //     var POPT1 = couples.Where(x => x.Course == 3 && x.Group == "ПО (ТО)");
+
+    //     Assert.Equal(25, POPT1.Count());
+    // }
+
+    public static List<ClassInfo> GetCouples(string path)
+    {
+        using var IFMOIOT = new TemplateScheduleParser(path);
+        return IFMOIOT.Parse();
+    }
+}
+
---
+++ UnitTests/Paths.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace job_checker.UnitTests
+{
+    public static class Paths
+    {
+        public static readonly string OFOBAK1 = "../../../UnitTests/TESTSDATA/OFOBAK1.xlsx";
+        public static readonly string OFOBAK2 = "../../../UnitTests/TESTSDATA/OFOBAK2.xlsx";
+        public static readonly string OFOBAK3 = "../../../UnitTests/TESTSDATA/OFOBAK3.xlsx";
+        public static readonly string OFOMAG = "../../../UnitTests/TESTSDATA/OFOMAG.xlsx";
+        public static readonly string ZFOBAK = "../../../UnitTests/TESTSDATA/ZFOBAK.xls";
+        public static readonly string ZFOMAG = "../../../UnitTests/TESTSDATA/ZFOMAG.xls";
+    }
+}
