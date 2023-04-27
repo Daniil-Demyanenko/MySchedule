cb73285f9411fe7fc816d0009bf4cf24c8317ff1
Daniil Demyanenko <daniil-demyanenko1@ya.ru>
2023-04-27 23:40:35 +0300

    ТГ бот: есть всё, кроме вывода пар

--- TelegramUI/TelegramBot.cs
+++ TelegramUI/TelegramBot.cs
@@ -8,91 +8,192 @@ using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Args;
 using Telegram.Bot.Exceptions;
 using System.Threading;
+using System.Text.RegularExpressions;
 
-namespace job_checker.TelegramUI
+namespace job_checker.TelegramUI;
+
+/// <summary>
+/// Класс, отвечающий за взаимодействие с пользователем
+/// </summary>
+public static class TelegramBot
 {
-    /// <summary>
-    /// Класс, отвечающий за взаимодействие с пользователем
-    /// </summary>
-    public static class TelegramBot
+    private static TelegramDBContext DB = new();
+
+    public static void Start(string token)
     {
-        private static Dictionary<ChatId, TelegramUser> _Users = new();
-        public static void Start(string token)
+        var TBClient = new TelegramBotClient(token);
+
+        TBClient.StartReceiving(HandleUpdate, HandleError);
+
+
+    }
+
+
+    private async static Task HandleUpdate(ITelegramBotClient TBClient, Update update, CancellationToken arg3)
+    {
+        if (!update.IsMessageType() && !update.IsCallbackType())
         {
-            var TBClient = new TelegramBotClient(token);
-
-            TBClient.StartReceiving(HandleUpdate, HandleError);
-
-
+            //await TBClient.SendTextMessageAsync(chatID, "Неподдерживаемый тип сообщений!");
+            return;
         }
 
-        private static Task HandleError(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        var user = GetUserFromDB(update.GetChatID());
+        var chatID = update.GetChatID();
+
+
+        if (update.IsMessageType() && update.Message.Text == "/start" && !user.IsRegistered())
         {
-            throw new NotImplementedException();
+            await TBClient.SendTextMessageAsync(chatID, "Этот бот служит для удобного получения расписания студентов ИФОИОТ.");
+            await RequestCourse(TBClient, chatID);
+            return;
         }
 
-        private async static Task HandleUpdate(ITelegramBotClient TBClient, Update update, CancellationToken arg3)
+        if (update.IsMessageType())
         {
-            var ikm = new InlineKeyboardMarkup(new[]
-            {
+            HandeBugreport(TBClient, update, user);
+            return;
+        }
+
+        // Далее мы точно уверены, что тип сообщения -- CallbackQuery
+
+        Match matches = Regex.Match(update.CallbackQuery.Data, "^[A-Z]+");
+        string command = matches.Value;
+
+        switch (command)
+        {
+            case "COURSE":
+                await HandeCourseSelection(TBClient, update, user);
+                return;
+            case "GROUP":
+                await HandeGroupSelection(TBClient, update, user);
+                return;
+            case "RESET":
+                await RestartRegistration(TBClient, chatID, user);
+                return;
+        }
+
+    }
+
+
+    private static async Task RequestCourse(ITelegramBotClient TBClient, long chatID)
+    {
+        var ikm = new InlineKeyboardMarkup(new[]
+       {
                 new[]
                 {
-                    InlineKeyboardButton.WithCallbackData(" 1 курс ", "1"),
-                    InlineKeyboardButton.WithCallbackData(" 2 курс ", "2"),
-                    InlineKeyboardButton.WithCallbackData(" 3 курс ", "3"),
+                    InlineKeyboardButton.WithCallbackData(" 1 курс ", "COURSE_1"),
+                    InlineKeyboardButton.WithCallbackData(" 2 курс ", "COURSE_2"),
+                    InlineKeyboardButton.WithCallbackData(" 3 курс ", "COURSE_3"),
                 },
                 new[]
                 {
-                    InlineKeyboardButton.WithCallbackData(" 4 курс ", "4"),
-                    InlineKeyboardButton.WithCallbackData(" 5 курс ", "5"),
+                    InlineKeyboardButton.WithCallbackData(" 4 курс ", "COURSE_4"),
+                    InlineKeyboardButton.WithCallbackData(" 5 курс ", "COURSE_5"),
                 },
-            });
+        });
+
+        await TBClient.SendTextMessageAsync(chatID, "Выберите курс:", replyMarkup: ikm);
+    }
+
+    private static async Task HandeCourseSelection(ITelegramBotClient TBClient, Update update, TelegramUser user)
+    {
+        int course = int.Parse(update.CallbackQuery.Data.Split('_', StringSplitOptions.RemoveEmptyEntries)[1]);
+
+        await TBClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id, $"Выбран {course} курс.");
+
+        user.Course = course;
+        DB.Update(user);
+        await DB.SaveChangesAsync();
+
+        await RequestPosibleGroup(TBClient, update.GetChatID(), course);
+    }
+
+    private static async Task RequestPosibleGroup(ITelegramBotClient TBClient, long chatID, int course)
+    {
+        var groups = CoupleSchedule.StudyGroups.Where(x => x.Course == course).Select(x => x.GroupName).Order().ToArray();
+        var buttons = new InlineKeyboardButton[groups.Count() + 1][];
+
+        for (int i = 0; i < groups.Count(); i++)
+            buttons[i] = new[] { InlineKeyboardButton.WithCallbackData(groups[i], $"GROUP_{groups[i]}") };
+
+        buttons[^1] = new[] { InlineKeyboardButton.WithCallbackData("Вернуться к выбору курса", "RESET") };
+
+        var keyboard = new InlineKeyboardMarkup(buttons);
+
+        await TBClient.SendTextMessageAsync(chatID, "Выберите группу:", replyMarkup: keyboard);
+    }
+
+    private static async Task HandeGroupSelection(ITelegramBotClient TBClient, Update update, TelegramUser user)
+    {
+        Match matches = Regex.Match(update.CallbackQuery.Data, "^GROUP_(.*)");
+        user.GroupName = matches.Groups[1].ToString();
+
+        await TBClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id, $"Выбрана группа {user.GroupName}.");
+
+        DB.Update(user);
+        await DB.SaveChangesAsync();
+    }
+
+    private static async Task RestartRegistration(ITelegramBotClient TBClient, long chatID, TelegramUser user)
+    {
+        await TBClient.SendTextMessageAsync(chatID, "Настройки пользователя сброшены.");
+
+        user.Course = null;
+        user.GroupName = null;
+        DB.Update(user);
+        await DB.SaveChangesAsync();
+
+        await RequestCourse(TBClient, chatID);
+    }
+
+    private static async Task HandeBugreport(ITelegramBotClient TBClient, Update update, TelegramUser user)
+    {
+        BugreportNote note = new(update.Message.From.FirstName,
+                                update.Message.From.LastName,
+                                update.Message.From.Username,
+                                update.Message.Text, user);
+
+        DB.Bugreports.Add(note);
+        await DB.SaveChangesAsync();
+
+        await TBClient.SendTextMessageAsync(update.GetChatID(), "Спасибо за отзыв! Постараемся решить в скорейшее время.");
+    }
+
+    private static async Task HandePrintCouplesSelection(ITelegramBotClient TBClient, Update update, int course) { }
 
 
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message && update.Message.Text == "/start")
-            {
-                await TBClient.SendTextMessageAsync(update.Message.Chat.Id, "Здарова меченый, ты на старт нажал и в благородство я играть не буду. Выберешь курс свой, потом группу и мы в расчёте.", replyMarkup: ikm);
-            }
-
-            int course;
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery && int.TryParse(update.CallbackQuery.Data, out course))
-            {
-                _Users.TryAdd(update.CallbackQuery.Message.Chat.Id, new TelegramUser() { Course = course});
-                var a = update.CallbackQuery.Data;
-
-                await PrintPosibleGroupsForUser(TBClient, update, course);
-
-                await TBClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id, $"Выбран {a} курс");
-            }
-            else if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery && update.CallbackQuery.Data == "start")
-            {
-                await TBClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id, "Выберите свой курс:", replyMarkup: ikm);
-            }
-            else if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery)
-            {
-                _Users[update.CallbackQuery.Message.Chat.Id].GroupName = update.CallbackQuery.Data;
 
 
+    private static TelegramUser GetUserFromDB(long chatID)
+    {
+        TelegramUser user = null;
+        if (DB.Users.Count() > 0)
+            user = DB.Users.Where(x => x.ChatID == chatID)?.First();
 
-            }
-
-        }
-
-        private static async Task PrintPosibleGroupsForUser(ITelegramBotClient TBClient, Update update, int course)
+        if (user is null)
         {
-
-            var groups = CoupleSchedule.StudyGroups.Where(x => x.Course == course).Select(x => x.GroupName).Order().ToArray();
-            var buttons = new InlineKeyboardButton[groups.Count() + 1][];
-            for (int i = 0; i < groups.Count(); i++)
-                buttons[i] = new[] { InlineKeyboardButton.WithCallbackData(groups[i], groups[i]) };
-
-            buttons[^1] = new[] { InlineKeyboardButton.WithCallbackData("Вернуться к выбору курса", "start") };
-
-            var keyboard = new InlineKeyboardMarkup(buttons);
-
-            await TBClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id, "Выберите группу:", replyMarkup: keyboard);
+            DB.Users.Add(new TelegramUser(chatID));
+            DB.SaveChanges();
+            user = DB.Users.Where(x => x.ChatID == chatID).First();
         }
 
+        return user;
+    }
+    private static long GetChatID(this Update update)
+    {
+        if (update.IsCallbackType())
+            return update.CallbackQuery.Message.Chat.Id;
+        return update.Message.Chat.Id;
+    }
 
+
+    private static bool IsRegistered(this TelegramUser user) => user.Course != null && user.GroupName != null;
+    private static bool IsMessageType(this Update update) => update.Type == Telegram.Bot.Types.Enums.UpdateType.Message;
+    private static bool IsCallbackType(this Update update) => update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery;
+
+
+    private static Task HandleError(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+    {
+        throw new NotImplementedException();
     }
 }
--- TelegramUI/TelegramUser.cs
+++ TelegramUI/TelegramUser.cs
@@ -6,11 +6,11 @@ public class TelegramUser
     [Key]
         public long Id { get; set; }
         public long ChatID { get; set; }
-        public int Course { get; set; }
+        public int? Course { get; set; }
         public string GroupName { get; set; }
         //public ICollection<BugreportNote> BugreportNotes { get; set; }
 
         public TelegramUser(long chatID, int course, string group)
         => (ChatID, Course, GroupName) = (chatID, course, group);
-        public TelegramUser() { }
+        public TelegramUser(long chatID) => ChatID = chatID;
 }
